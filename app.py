235829a9194404d1b3740e0b4cedd0b5c67c692f
Allison Valdez <allison.snipe@gmail.com>
2026-05-16 14:19:09 -0700

    test

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
+# Import all utilities
 from flask import Flask, jsonify, request
 from data import products
 
 app = Flask(__name__)
 
-# TODO: Implement homepage route that returns a welcome message
-
-@app.route("/")
+"""
+Implement homepage route that returns a welcome message, don't forget to include the status message upon successful load.
+"""
+@app.route("/", methods={"GET"})
 def home():
-    pass  # TODO: Return a welcome message
+    # Return a welcome message
+    return jsonify({"message": "Welcome to the homepage"}), 200
 
-# TODO: Implement GET /products route that returns all products or filters by category
+"""
+Implement GET /products route that returns all products or filters by category
+"""
 
-@app.route("/products")
+@app.route("/products", methods=["GET"])
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+    
+    # Return all products or filter by ?category=
+    category = request.args.get("category")
 
-# TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
+    # Control flow
+    if category:
+        filtered = [item for item in data if item["category"] == category]
+        return jsonify(filtered), 200
+    return jsonify(data), 200
 
-@app.route("/products/<int:id>")
+"""
+Implement GET /products/<id> route that returns a specific product by ID or 404. For a unique identifier.
+"""
+@app.route("/products/<int:id>", methods={"GET"})
 def get_product_by_id(id):
-    pass  # TODO: Return product by ID or 404
 
+    # Return product by ID or 404
+    for product in data:
+        # Control flow
+        if product["id"] == id:
+            return jsonify(product), 200
+        return jsonify({"message": "Product not found"}), 404
+    
 if __name__ == "__main__":
     app.run(debug=True)
